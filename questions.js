f0c03ab9b963555901ab32e3fc50c73f4e8b002b
Abdullah Zuberi <111603644+Abdullah-Zuberi@users.noreply.github.com>
2024-02-21 14:13:41 +0500

    commit message

---
+++ questions.js
@@ -0,0 +1,39 @@
+const questions=[
+    {
+         "id":1,
+         "question":"Who is the founder of Pakistan?",
+         "options":["Quaid-e-Azam Muhammad Ali Jinnah", "Allama Iqbal","Benazir Bhutto","No idea"],
+         "answer": 0
+     },
+     {
+         "id":2,
+         "question":"Where is Mohenjo-Daro located?",
+         "options":["Punjab", "Sindh","Balochistan", "No idea"],
+         "answer": 1
+     },
+     {
+         "id":3,
+         "question":"What river flows through the city of Lahore?",
+         "options":["Indus", "Ravi","Jhelum", "No idea"],
+         "answer": 1
+     },
+     {
+         "id":4,
+         "question":"Which mountain range is located in Pakistan?",
+         "options":["Rocky Mountains", "Himalayas","Andes", "Alps"],
+         "answer": 1
+     },
+     {
+         "id":5,
+         "question":"Who is known as the poet of the East in Pakistan?",
+         "options":["Faiz Ahmed Faiz", "Mirza Ghalib","Allama Iqbal", "No idea"],
+         "answer": 2
+     },
+     {
+         "id":6,
+         "question":"What is the national dress of Pakistan?",
+         "options":["Sari", "Kurta and Shalwar","Kimono", "No idea"],
+         "answer": 1
+     }
+ ]
+ 
